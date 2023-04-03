195098083f55de0cd76e0ec747cb3ba803771357
Liam Osler <osler.liam@gmail.com>
2023-04-02 23:05:16 -0300

    working customer update and some other fixes

--- models/Users.js
+++ models/Users.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   username: String,
   email: String,
-  about: String,
+  password: String,
 }, {timestamps: true});
 
 module.exports = mongoose.model("users", schema)
